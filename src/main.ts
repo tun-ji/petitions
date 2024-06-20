8557f174890f7e55826f4842c83c5797cd908d0f
tun-ji <oluwatomilola.adeniran@pau.edu.ng>
2024-06-20 17:35:13 +0100

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     },
   });
   await app.startAllMicroservices();
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(10000, '0.0.0.0');
 }
 bootstrap();
